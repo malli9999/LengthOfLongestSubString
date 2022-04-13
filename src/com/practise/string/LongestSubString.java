d4be01d269de2e6da190d6d2cea6e2d8ab7731ab
Arhun <bmallikarujuna66@gmail.com>
2022-04-12 20:59:32 -0500

    Added the Java skeleton

---
+++ src/com/practise/string/LongestSubString.java
@@ -0,0 +1,14 @@
+package com.practise.string;
+
+public class LongestSubString {
+
+	public LongestSubString() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
